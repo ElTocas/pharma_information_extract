f6d238a586bf4e3e02c99fd1f80d7566591d7188
ElTocas <39244219+ElTocas@users.noreply.github.com>
2021-05-20 23:49:04 +0200

    Riduco ricerca
    Ridotto ricerca per tipo di farmaco e malattie respiratorie

--- estrapolarelistaaziende.py
+++ estrapolarelistaaziende.py
@@ -11,15 +11,25 @@ import numpy as np
 Tab_aifa = pd.read_csv('Classe_A.csv',sep='\t')
 
 parola_da_cerca = "Ciclesonide";
-parole_da_cerca = pd.Series(["ciclesonide", "fluticasone","olodaterolo","tiotropio","formoterolo","beclometasone","budesonide","indacaterolo","glicopirronio","aclidinio","benlarizumab"]);
+#parole_da_cerca = pd.Series(["ciclesonide", "fluticasone","olodaterolo","tiotropio","formoterolo","beclometasone","budesonide","indacaterolo","glicopirronio","aclidinio","benlarizumab"]);
 
+parole_da_cerca = pd.Series(["fluticasone", "beclometasone","budesonide"]);
 principio_attivo=Tab_aifa["Principio Attivo"].str.lower()
 
 
 indice=np.where(principio_attivo.isin(parole_da_cerca))
 
 Tab_aifa_red = Tab_aifa.iloc[indice[0],:]
-aziende = Tab_aifa["Titolare AIC"][indice[0]]
+
+denominazione=Tab_aifa_red["Descrizione Gruppo Equivalenza"].str.lower()
+
+indice=np.where(denominazione.str.contains('respiratorio'))
+
+Tab_aifa_red = Tab_aifa_red.iloc[indice[0],:]
+
+
+
+aziende = Tab_aifa_red["Titolare AIC"]
 aziende = aziende.unique()
 
 
@@ -28,3 +38,5 @@ dizionario = dict()
 for azienda in aziende:
     temp = Tab_aifa_red[Tab_aifa_red["Titolare AIC"] == azienda]
     dizionario[azienda]=temp
+
+
